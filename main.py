3705d2ab4648825642a40a9a1c92c6032c67608b
Fares Setbel <fares.setbels@gmail.com>
2022-03-03 16:50:11 +0100

    Now a mix between Cons and Voys

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'y', 'v' 'w', 'x', 'y', 'z']
-voyels = ['a', 'i', 'u' 'e', 'o']
+voyels = ['a', 'i', 'u' 'e', 'o', 'o', 'io']
+
+types = [consonants, voyels]
 
 random.shuffle(consonants)
 
 usr_len = 5
 i = 0
+random.shuffle(types)
 while i <= usr_len:
     random.shuffle(consonants)
-    print(consonants[0])
+    print(types[0][0])
     random.shuffle(voyels)
-    print(voyels[0])
+    print(types[1][0])
     i += 1
