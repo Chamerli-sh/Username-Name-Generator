ed0828850b560062551b1f0768153e421a1074eb
Fares Setbel <fares.setbels@gmail.com>
2022-03-03 16:45:02 +0100

    Randomize

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-
+import random
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'y', 'v' 'w', 'x', 'y', 'z']
 voyels = ['a', 'i', 'u' 'e', 'o']
+
+random.shuffle(consonants)
+
+usr_len = 5
+i = 0
+while i <= usr_len:
+    random.shuffle(consonants)
+    print(consonants[0])
+    random.shuffle(voyels)
+    print(voyels[0])
+    i += 1
