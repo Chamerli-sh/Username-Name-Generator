d49b06f373489a635a04aa8e2c7942089105865d
Chamerli-sh <fares.setbels@gmail.com>
2022-03-04 11:11:30 +0100

    Username var

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import random
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'y', 'v' 'w', 'x', 'y', 'z']
-voyels = ['a', 'i', 'u' 'e', 'o', 'o', 'io']
+voyels = ['a', 'i', 'u' 'e', 'o', 'oo', 'io', 'oy', 'ai', 'ui']
 
 types = [consonants, voyels]
 
 random.shuffle(consonants)
-
-usr_len = 5
+username = ""
+usr_len = int(input("Enter the lenght of your usename: "))
 i = 0
 random.shuffle(types)
 while i <= usr_len:
     random.shuffle(consonants)
-    print(types[0][0])
+    username += types[0][0]
+
     random.shuffle(voyels)
-    print(types[1][0])
+    username += types[1][0]
     i += 1
+
+print(username)
